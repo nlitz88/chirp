b8c930ac0ce92d2d6a4e7952e1cfc2c4862ca71d
Nathan Litzinger <nlitz88@gmail.com>
2024-01-10 19:38:31 -0500

    Setup poetry project and added core modules

---
+++ chirp/pipeline.py
@@ -0,0 +1,8 @@
+"""Contains code implementing the core object detection + object tracking bird
+feeder analysis pipeline. Uses input video feed from a file or video device and
+outputs to an RTMP server.
+"""
+
+def detect_track_stream():
+    pass
+
---
+++ chirp/scheduler.py
@@ -0,0 +1,4 @@
+"""Module containing code to start/stop different components of the chirp bird
+stream analysis system. For now, this is essentially a parent process that just
+starts/stops the stream at a designated time.
+"""
