9542d210a72234257490de70ae2ff81a3a378da7
Nathan Litzinger <nlitz88@gmail.com>
2024-01-12 01:41:08 -0500

    Added object detection annotations to images

--- chirp/birdcounter.py
+++ chirp/birdcounter.py
@@ -11,6 +11,7 @@ import numpy as np
 import cv2 as cv
 
 from ultralytics import YOLO
+import supervision as sv
 
 def main():
 
@@ -30,10 +31,14 @@ def main():
     video_source_path = Path(args.video_source)
     if not video_source_path.exists():
         raise FileNotFoundError(f"Failed to find video source at provided path: {video_source_path}")
-
     model_weights_path = Path(args.yolo_weights)
     if not model_weights_path.exists():
         raise FileNotFoundError(f"YOLOv8 model weights not found at provided path: {model_weights_path}")
+
+    # TODO: Create a "BirdCounter" class of some sort where, given the parsed
+    # arguments from whatever interface you're using above (like a CLI), you
+    # instantiate a new BirdCounter instance and then run the stream as a method
+    # of that class.
 
     # Set camera parameters.
     feeder_camera = cv.VideoCapture(str(video_source_path))
@@ -44,6 +49,10 @@ def main():
 
     # Load YOLO model weights.
     model = YOLO(model=model_weights_path)
+
+    # Create a supervision bounding box annotator.
+    bounding_box_annotator = sv.BoundingBoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
 
     while True:
         status, frame = feeder_camera.read()
@@ -59,13 +68,26 @@ def main():
         preview_width = frame_width // 2
 
         # Run inference on the captured frame.
-        result = model(frame)
+        result = model(frame)[0] # Only want the results for the single frame passed in.
+        # Convert the results from ultralytics format to supervision's format.
+        detections = sv.Detections.from_ultralytics(ultralytics_results=result)
+        NMS_THRESHOLD = 0.5
+        filtered_detections = detections.with_nms(threshold=NMS_THRESHOLD)
+
+        # Create labels maintained by model.
+        # https://supervision.roboflow.com/how_to/detect_and_annotate/#annotate-image
+        labels = [model.names[class_id] for class_id in detections.class_id]
 
         # Add annotations to frame using supervision.
+        annotated_image = bounding_box_annotator.annotate(scene=frame,
+                                                          detections=detections)
+        annotated_image = label_annotator.annotate(scene=annotated_image,
+                                                   detections=detections,
+                                                   labels=labels)
 
         # If successfully retrieved, display the retrieved frame. Create a
         # duplicate frame resized for viewing output.
-        preview_frame = cv.resize(src=frame, dsize=(preview_width, preview_height))
+        preview_frame = cv.resize(src=annotated_image, dsize=(preview_width, preview_height))
 
         # Display the resized, annotated frame.
         cv.imshow(winname="Camera Frames", mat=preview_frame)
