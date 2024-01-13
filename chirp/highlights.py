7c94c1026431469e37d088b546bbe00da9da3a9f
Nathan Litzinger <nlitz88@gmail.com>
2024-01-13 13:12:49 -0500

    Stubbed out a module for generating highlight reels

---
+++ chirp/highlights.py
@@ -0,0 +1,4 @@
+"""Module containing functions to identify time ranges in a specified session or
+time range with desirable/high bird activity and functions to cut together clips
+from the corresponding video files to generate a highlight reel.
+"""
