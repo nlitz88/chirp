95aec5bd8ef2e9a18990c527469ebf38d3c56bcd
Nathan Litzinger <nlitz88@gmail.com>
2024-01-13 14:55:24 -0500

    Added additional planning to zone monitor

--- chirp/zone_monitor.py
+++ chirp/zone_monitor.py
@@ -25,12 +25,38 @@ class ZoneMonitor:
         # Grab constructor parameters.
         self._in_threshold = in_threshold
         self._out_timeout = out_timeout
-        # Create dictionary for tracked detections. Maps a detection's
+        # Create dictionary for monitored detections. Maps a detection's
         # tracker_id to the number of times it has been seen and its timeout.
-        tracked_detections = {}
+        self._monitored_detections = {}
 
     def update(detections_in_zone: sv.Detections) -> Tuple[sv.Detections, sv.Detections]:
         
+        # Rough idea:
+        # For each of the detections, check if it's TRACKER_ID is already in the
+        # tracked_detections dictionary.
+
+        # If it is, increment its frames_since_added counter. Maybe update with
+        # the ceil of the threshold so the numbers don't overflow.
+        
+        # If it's not already in there, create a new entry in the dictionary for
+        # it. The entry should map the tracker id to a new dictionary, whose
+        # values will be the detection vector, the frames_since_added_counter,
+        # and the out_timeout_counter.
+
+        
+        # Also, have to check for existing detections, if any of their
+        # out_timeout_counter values have reached 0, if their frames_since_added
+        # == in_threshold, then that entry can be removed from the dictionary
+        # and returned as an EXITED detection.
+
+        # If it's frames_since_added is not == in_threshold, then that detection
+        # wasn't around long enough to be considered present, and therefore
+        # shouldn't be returned as exited or counted.
+
+        # Also, have to check for existing detections, if their
+        # frames_since_added reaches the in_threshold, then those instances can
+        # be returned in the list of "entered detections" from this call to
+        # update.
         
 
         pass
